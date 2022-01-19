f85362a26d1068db681b92bea72b8e709f735ea8
Aviral Srivastava <aviralji4@gmail.com>
2022-01-19 17:08:42 +0530

    Update main.py

--- testing_kMeansClustering/main.py
+++ testing_kMeansClustering/main.py
@@ -5,6 +5,7 @@ from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 from sklearn.ensemble import ExtraTreesClassifier
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 data = pd.read_csv("mod_dataset/mod_data.csv")
 
@@ -31,15 +32,17 @@ data[data < 0] = 0
 
 X = data.iloc[:,0:47]  #independent columns
 y = data.iloc[:,-1]    #target column
+print(X)
 
-# bestfeatures = SelectKBest(score_func=chi2, k=20)
-# fit = bestfeatures.fit(X,y)
-# dfscores = pd.DataFrame(fit.scores_)
-# dfcolumns = pd.DataFrame(X.columns)
-# #concat two dataframes for better visualization 
-# featureScores = pd.concat([dfcolumns,dfscores],axis=1)
-# featureScores.columns = ['Specs','Score']  #naming the dataframe columns
-# print(featureScores.nlargest(20,'Score'))  #print 10 best features
+bestfeatures = SelectKBest(score_func=chi2, k=20)
+fit = bestfeatures.fit(X,y)
+dfscores = pd.DataFrame(fit.scores_)
+dfcolumns = pd.DataFrame(X.columns)
+#concat two dataframes for better visualization 
+featureScores = pd.concat([dfcolumns,dfscores],axis=1)
+featureScores.columns = ['Specs','Score']  #naming the dataframe columns
+# print(featureScores.nlargest(20,'Score'))
+# print(featureScores)
 
 
 # model = ExtraTreesClassifier()
@@ -50,10 +53,9 @@ y = data.iloc[:,-1]    #target column
 # plt.show()
 
 
-#get correlations of each features in dataset
-corrmat = data.corr()
-top_corr_features = corrmat.index
-plt.figure(figsize=(20,20))
-#plot heat map
-g=sns.heatmap(data[top_corr_features].corr(),annot=True,cmap="RdYlGn")
+# corrmat = data.corr()
+# top_corr_features = corrmat.index
+# plt.figure(figsize=(20,20))
+# g=sns.heatmap(data[top_corr_features].corr(),annot=True,cmap="RdYlGn")
+
 

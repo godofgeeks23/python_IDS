a435cd51ee380e5723497a311f015b8cf9219d71
Aviral Srivastava <aviralji4@gmail.com>
2022-01-19 11:56:13 +0530

    Method 1 Failed

--- failed_supervisedTimeseries/main.py
+++ failed_supervisedTimeseries/main.py
@@ -1,3 +1,5 @@
+from pycaret.datasets import get_data
 import pandas as pd
+
 data = pd.read_csv('mod_dataset\mod_dataset.csv')
-print(data)
+print(data.head())
---
+++ testing_kMeansClustering/main.py
@@ -0,0 +1,5 @@
+from pycaret.datasets import get_data
+import pandas as pd
+
+data = pd.read_csv('mod_dataset\mod_dataset.csv')
+print(data.head())

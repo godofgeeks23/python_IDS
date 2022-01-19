362a18ebf432211fd4f0d07160f31c10c7e125eb
Aviral Srivastava <aviralji4@gmail.com>
2022-01-19 16:44:08 +0530

    Update main.py

--- testing_kMeansClustering/main.py
+++ testing_kMeansClustering/main.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
+from sklearn.ensemble import ExtraTreesClassifier
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("mod_dataset/mod_data.csv")
 
@@ -24,16 +27,33 @@ data = data.drop('DATETIME', 1)
 
 data = data.apply(pd.to_numeric)
 data[data < 0] = 0
+
+
 X = data.iloc[:,0:47]  #independent columns
-y = data.iloc[:,-1]    #target column, apply SelectKBest class to extract top 10 best features
+y = data.iloc[:,-1]    #target column
 
-bestfeatures = SelectKBest(score_func=chi2, k=20)
-fit = bestfeatures.fit(X,y)
-dfscores = pd.DataFrame(fit.scores_)
-dfcolumns = pd.DataFrame(X.columns)
+# bestfeatures = SelectKBest(score_func=chi2, k=20)
+# fit = bestfeatures.fit(X,y)
+# dfscores = pd.DataFrame(fit.scores_)
+# dfcolumns = pd.DataFrame(X.columns)
+# #concat two dataframes for better visualization 
+# featureScores = pd.concat([dfcolumns,dfscores],axis=1)
+# featureScores.columns = ['Specs','Score']  #naming the dataframe columns
+# print(featureScores.nlargest(20,'Score'))  #print 10 best features
 
-#concat two dataframes for better visualization 
-featureScores = pd.concat([dfcolumns,dfscores],axis=1)
 
-featureScores.columns = ['Specs','Score']  #naming the dataframe columns
-print(featureScores.nlargest(20,'Score'))  #print 10 best features
+# model = ExtraTreesClassifier()
+# model.fit(X,y)
+# print(model.feature_importances_)
+# feat_importances = pd.Series(model.feature_importances_, index=X.columns)
+# feat_importances.nlargest(10).plot(kind='barh')
+# plt.show()
+
+
+#get correlations of each features in dataset
+corrmat = data.corr()
+top_corr_features = corrmat.index
+plt.figure(figsize=(20,20))
+#plot heat map
+g=sns.heatmap(data[top_corr_features].corr(),annot=True,cmap="RdYlGn")
+

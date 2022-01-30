10c67d5ed369455c9d269dbdb33182c9a7a741cd
Aviral Srivastava <aviralji4@gmail.com>
2022-01-30 19:15:45 +0530

    New models

--- testing_kMeansClustering/IDS_test.py
+++ testing_kMeansClustering/IDS_test.py
@@ -44,7 +44,9 @@ X = data.iloc[:, data.columns != 'target']  # independent columns
 y = data.iloc[:, -1]  # target column
 
 # best feature selection - by univariate selection
-vari = 10   # number of features to select
+vari = 5   # number of features to select
+max_accuracy = 0
+
 bestfeatures = SelectKBest(score_func=chi2, k=vari)
 fit = bestfeatures.fit(X, y)
 dfscores = pd.DataFrame(fit.scores_)
@@ -52,57 +54,59 @@ dfcolumns = pd.DataFrame(X.columns)
 featureScores = pd.concat([dfcolumns, dfscores], axis=1)
 featureScores.columns = ['Feature', 'Score']
 
-# # reducing the whole dataset to the selected features
-# new_data_X = pd.DataFrame(
-#     data[list(featureScores.nlargest(vari, 'Score')['Feature'])])
-# new_data_Y = pd.DataFrame(data['target'])
-# new_data = pd.concat([new_data_X, new_data_Y], axis=1)
-# # print(new_data.head())
+# reducing the whole dataset to the selected features
+new_data_X = pd.DataFrame(
+    data[list(featureScores.nlargest(vari, 'Score')['Feature'])])
+new_data_Y = pd.DataFrame(data['target'])
+new_data = pd.concat([new_data_X, new_data_Y], axis=1)
+# print(new_data.head())
 
-# # training the model
-# traintesttratio = 0.02
-# max_accuracy = 0
-# while(traintesttratio < 1):
-#     dissection = int(len(new_data) * traintesttratio) # number of rows to be used for training
-#     train_data = new_data.iloc[0:dissection, :]   # training data
-#     train_data_X = train_data.iloc[:, train_data.columns != 'target'] # independent columns
-#     train_data_Y = train_data.iloc[:, -1]   # target column
+# training the model
+print("training for vari = " + str(vari))
+traintesttratio = 0.02
+while(traintesttratio < 1):
+    dissection = int(len(new_data) * traintesttratio) # number of rows to be used for training
+    train_data = new_data.iloc[0:dissection, :]   # training data
+    train_data_X = train_data.iloc[:, train_data.columns != 'target'] # independent columns
+    train_data_Y = train_data.iloc[:, -1]   # target column
 
-#     test_data = new_data.iloc[dissection:, :] # testing data
-#     test_data_X = test_data.iloc[:, test_data.columns != 'target']    # independent columns
-#     test_data_Y = test_data.iloc[:, -1]   # target column
+    test_data = new_data.iloc[dissection:, :] # testing data
+    test_data_X = test_data.iloc[:, test_data.columns != 'target']    # independent columns
+    test_data_Y = test_data.iloc[:, -1]   # target column
 
-#     kmeans = KMeans(2)    # create an instance of the model (2 clusters)
-#     kmeans.fit(train_data_X, train_data_Y)    # train the model
-#     predictions = kmeans.predict(test_data_X) # test the model
+    kmeans = KMeans(2)    # create an instance of the model (2 clusters)
+    kmeans.fit(train_data_X, train_data_Y)    # train the model
+    predictions = kmeans.predict(test_data_X) # test the model
 
-#     # # print(classification_report(test_data_Y, predictions))
-#     traintesttratio += 0.001  # increase the traintesttratio
-#     if(accuracy_score(test_data_Y, predictions) > max_accuracy):  # if the accuracy is better than the previous one
-#         max_accuracy = accuracy_score(test_data_Y, predictions)   # save the new accuracy
-#         best_kmeans = kmeans  # save the model
-# print('Accuracy achieved: {}'.format(max_accuracy*100))   # print the accuracy
-# picklefilename = "best_model_nontime{}.pickle".format(max_accuracy*100)   # save the best model
-# with open(picklefilename, "wb") as file:
-#     pickle.dump(best_kmeans, file)    
+    # print(classification_report(test_data_Y, predictions))
+    traintesttratio += 0.001  # increase the traintesttratio
+    if(accuracy_score(test_data_Y, predictions) > max_accuracy):  # if the accuracy is better than the previous one
+        max_accuracy = accuracy_score(test_data_Y, predictions)   # save the new accuracy
+        best_kmeans = kmeans  # save the model
+        
+print('Accuracy achieved: {}'.format(max_accuracy*100))   # print the accuracy
+picklefilename = "best_model_nontime{}.pickle".format(max_accuracy*100)   # save the best model
+with open(picklefilename, "wb") as file:
+    pickle.dump(best_kmeans, file)
 
-# the model has been trained from the above code. now we need to use it to predict for the file that was passed as an argument
-try:    # try to open the file
-    resultinput = open(sys.argv[1], "rb")   # open the file
-    print("File opened")
-    resultinput.close()
 
-    resultinputfile = pd.read_csv(sys.argv[1])  # read the file
-    resultinputdata = pd.DataFrame(resultinputfile[list(featureScores.nlargest(vari, 'Score')['Feature'])]) # select the best features (data refining)
-    with open('best_model_nontime92.3076923076923.pickle', "rb") as file:   # load the best saved model
-        saved_model = pickle.load(file)
-    finalresult = pd.DataFrame(resultinputfile['INDEX(TIME_IN_HOURS)']) # create a dataframe for the result
-    finalresult['TIME'] = finalresult['INDEX(TIME_IN_HOURS)']
-    finalresult.drop('INDEX(TIME_IN_HOURS)', 1, inplace=True)
-    finalresult['LABEL'] = saved_model.predict(resultinputdata)    # predict the result
-    finalresult['LABEL'].replace(to_replace=0, value='NORMAL', inplace=True)    # replace the 0 values with NORMAL
-    finalresult['LABEL'].replace(to_replace=1, value='ATTACK', inplace=True)    # replace the 1 values with ATTACK
-    finalresult.to_csv("result.csv", index=False)  # save the result
+# # the model has been trained from the above code. now we need to use it to predict for the file that was passed as an argument
+# try:    # try to open the file
+#     resultinput = open(sys.argv[1], "rb")   # open the file
+#     print("File opened")
+#     resultinput.close()
 
-except FileNotFoundError:   # if the file is not found
-    print("specified file was not found")
+#     resultinputfile = pd.read_csv(sys.argv[1])  # read the file
+#     resultinputdata = pd.DataFrame(resultinputfile[list(featureScores.nlargest(vari, 'Score')['Feature'])]) # select the best features (data refining)
+#     with open('best_model_nontime92.3076923076923.pickle', "rb") as file:   # load the best saved model
+#         saved_model = pickle.load(file)
+#     finalresult = pd.DataFrame(resultinputfile['INDEX(TIME_IN_HOURS)']) # create a dataframe for the result
+#     finalresult['TIME'] = finalresult['INDEX(TIME_IN_HOURS)']
+#     finalresult.drop('INDEX(TIME_IN_HOURS)', 1, inplace=True)
+#     finalresult['LABEL'] = saved_model.predict(resultinputdata)    # predict the result
+#     finalresult['LABEL'].replace(to_replace=0, value='NORMAL', inplace=True)    # replace the 0 values with NORMAL
+#     finalresult['LABEL'].replace(to_replace=1, value='ATTACK', inplace=True)    # replace the 1 values with ATTACK
+#     finalresult.to_csv("result.csv", index=False)  # save the result
+
+# except FileNotFoundError:   # if the file is not found
+#     print("specified file was not found")
